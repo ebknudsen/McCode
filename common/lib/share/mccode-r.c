adc629bdc08e80dc4faaed765350cf6fcba9d584
Peter Willendrup <pkwi@fysik.dtu.dk>
2018-03-14 10:57:14 +0100

    Fixes to runtime and main loop in separate file

--- common/lib/share/mccode-r.c
+++ common/lib/share/mccode-r.c
@@ -60,9 +60,12 @@ static   long mcseed                 = 0; /* seed for random generator */
 static   long mcstartdate            = 0; /* start simulation time */
 static   int  mcdisable_output_files = 0; /* --no-output-files */
 mcstatic int  mcgravitation          = 0; /* use gravitation flag, for PROP macros */
+#pragma acc declare create ( mcgravitation )
 int      mcMagnet                    = 0; /* magnet stack flag */
+#pragma acc declare create ( mcMagnet )
 mcstatic int  mcdotrace              = 0; /* flag for --trace and messages for DISPLAY */
 int      mcallowbackprop             = 0;         /* flag to enable negative/backprop */
+#pragma acc declare create ( mcallowbackprop )
 
 /* Number of particle histories to simulate. */
 #ifdef NEUTRONICS
@@ -3431,6 +3434,21 @@ double randminmax(double min, double max) {
 	return rand0max(max - min) + max;
 }
 
+/**
+ * Wrapper functions for fprintf / printf on GPU
+ */
+
+#pragma acc routine seq
+int printf_GPU(const char *format,...) {
+  return 0;
+}
+
+#pragma acc routine seq
+int fprintf_GPU(FILE *stream, const char *format, ...) {
+  return 0;
+}
+
+
 /* SECTION: main and signal handlers ======================================== */
 
 /*******************************************************************************
@@ -3894,176 +3912,6 @@ void sighandler(int sig)
 
 } /* sighandler */
 #endif /* !NOSIGNALS */
-
-/*******************************************************************************
-* mccode_main: McCode main() function.
-*******************************************************************************/
-int mccode_main(int argc, char *argv[])
-{
-/*  double run_num = 0; */
-  time_t  t;
-  clock_t ct;
-#ifdef USE_MPI
-  char mpi_node_name[MPI_MAX_PROCESSOR_NAME];
-  int  mpi_node_name_len;
-#endif /* USE_MPI */
-
-#ifdef MAC
-  argc = ccommand(&argv);
-#endif
-
-#ifdef USE_MPI
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &mpi_node_count); /* get number of nodes */
-  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_node_rank);
-  MPI_Comm_set_name(MPI_COMM_WORLD, mcinstrument_name);
-  MPI_Get_processor_name(mpi_node_name, &mpi_node_name_len);
-#endif /* USE_MPI */
-
-ct = clock();    /* we use clock rather than time to set the default seed */
-mcseed=(long)ct;
-
-#ifdef USE_MPI
-/* *** print number of nodes *********************************************** */
-  if (mpi_node_count > 1) {
-    MPI_MASTER(
-    printf("Simulation '%s' (%s): running on %i nodes (master is '%s', MPI version %i.%i).\n",
-      mcinstrument_name, mcinstrument_source, mpi_node_count, mpi_node_name, MPI_VERSION, MPI_SUBVERSION);
-    );
-    /* share the same seed, then adapt random seed for each node */
-    MPI_Bcast(&mcseed, 1, MPI_LONG, 0, MPI_COMM_WORLD); /* root sends its seed to slaves */
-    mcseed += mpi_node_rank; /* make sure we use different seeds per noe */
-  }
-#endif /* USE_MPI */
-  srandom(mcseed);
-  mcstartdate = (long)t;  /* set start date before parsing options and creating sim file */
-
-/* *** parse options ******************************************************* */
-  SIG_MESSAGE("[" __FILE__ "] main START");
-  mcformat=getenv(FLAVOR_UPPER "_FORMAT") ?
-           getenv(FLAVOR_UPPER "_FORMAT") : FLAVOR_UPPER;
-  mcinstrument_exe = argv[0]; /* store the executable path */
-  /* read simulation parameters and options */
-  mcparseoptions(argc, argv); /* sets output dir and format */
-  
-#ifdef USE_MPI
-  if (mpi_node_count > 1) {
-    /* share the same seed, then adapt random seed for each node */
-    MPI_Bcast(&mcseed, 1, MPI_LONG, 0, MPI_COMM_WORLD); /* root sends its seed to slaves */
-    mcseed += mpi_node_rank; /* make sure we use different seeds per node */
-  }
-#endif
-  srandom(mcseed);
-
-/* *** install sig handler, but only once !! after parameters parsing ******* */
-#ifndef NOSIGNALS
-#ifdef SIGQUIT
-  if (signal( SIGQUIT ,sighandler) == SIG_IGN)
-    signal( SIGQUIT,SIG_IGN);   /* quit (ASCII FS) */
-#endif
-#ifdef SIGABRT
-  if (signal( SIGABRT ,sighandler) == SIG_IGN)
-    signal( SIGABRT,SIG_IGN);   /* used by abort, replace SIGIOT in the future */
-#endif
-#ifdef SIGTERM
-  if (signal( SIGTERM ,sighandler) == SIG_IGN)
-    signal( SIGTERM,SIG_IGN);   /* software termination signal from kill */
-#endif
-#ifdef SIGUSR1
-  if (signal( SIGUSR1 ,sighandler) == SIG_IGN)
-    signal( SIGUSR1,SIG_IGN);   /* display simulation status */
-#endif
-#ifdef SIGUSR2
-  if (signal( SIGUSR2 ,sighandler) == SIG_IGN)
-    signal( SIGUSR2,SIG_IGN);
-#endif
-#ifdef SIGHUP
-  if (signal( SIGHUP ,sighandler) == SIG_IGN)
-    signal( SIGHUP,SIG_IGN);
-#endif
-#ifdef SIGILL
-  if (signal( SIGILL ,sighandler) == SIG_IGN)
-    signal( SIGILL,SIG_IGN);    /* illegal instruction (not reset when caught) */
-#endif
-#ifdef SIGFPE
-  if (signal( SIGFPE ,sighandler) == SIG_IGN)
-    signal( SIGSEGV,SIG_IGN);    /* floating point exception */
-#endif
-#ifdef SIGBUS
-  if (signal( SIGBUS ,sighandler) == SIG_IGN)
-    signal( SIGSEGV,SIG_IGN);    /* bus error */
-#endif
-#ifdef SIGSEGV
-  if (signal( SIGSEGV ,sighandler) == SIG_IGN)
-    signal( SIGSEGV,SIG_IGN);   /* segmentation violation */
-#endif
-#endif /* !NOSIGNALS */
-  mcsiminfo_init(NULL); /* open SIM */
-  SIG_MESSAGE("[" __FILE__ "] main INITIALISE");
-  mcinit();
-#ifndef NOSIGNALS
-#ifdef SIGINT
-  if (signal( SIGINT ,sighandler) == SIG_IGN)
-    signal( SIGINT,SIG_IGN);    /* interrupt (rubout) only after INIT */
-#endif
-#endif /* !NOSIGNALS */
-
-/* ================ main particle generation/propagation loop ================ */
-#if defined (USE_MPI)
-  /* sliced Ncount on each MPI node */
-  mcncount = mpi_node_count > 1 ?
-    floor(mcncount / mpi_node_count) :
-    mcncount; /* number of rays per node */
-#endif
-
-/* main particle event loop */
-/* main particle event loop */
-#include <openacc.h>
-  acc_attach( (void**)&mcinstance_origin );
-  acc_attach( (void**)&mcinstrument );
-
-#pragma acc parallel loop
-  /* old init: mcsetstate(0, 0, 0, 0, 0, 1, 0, sx=0, sy=1, sz=0, 1); */
-  for (unsigned long long Xmcrun_num=0 ; Xmcrun_num < mcncount ; Xmcrun_num++) {
-
-/* Initialise RNG in CUDA case */
-#if MC_ALG_RAND == 5 
-      curandState_t MCRANDstate;
-      long long seq = Xmcrun_num;
-      curand_init(mcseed, seq, 0ULL, MCRANDstate);
-#undef random
-#define random twister_initdraw(mcseed,particleN.uid,particleN.MCRANDstate);
-#endif
-/* End RNG in CUDA case */
-
-      mcparticle particleN = mcgenstate(); // initial particle
-      particleN.uid = Xmcrun_num;
-/* CUDA */
-#if MC_ALG_RAND == 5 
-      particleN.MCRANDstate = MCRANDstate;
-#endif
-      mcraytrace(particleN);
-    }
-  /* Likely we need an undef random here... */
-
-#ifdef USE_MPI
- /* merge run_num from MPI nodes */
-  if (mpi_node_count > 1) {
-  double mcrun_num_double = (double)mcrun_num;
-  mc_MPI_Sum(&mcrun_num_double, 1);
-  mcrun_num = (unsigned long long)mcrun_num_double;
-  }
-#endif
-
-/* save/finally executed by master node/thread */
-  mcfinally();
-
-#ifdef USE_MPI
-  MPI_Finalize();
-#endif /* USE_MPI */
-
-  return 0;
-} /* mccode_main */
 
 #ifdef NEUTRONICS
 /*Main neutronics function steers the McStas calls, initializes parameters etc */
