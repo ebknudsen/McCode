66e70959bd60e5c80e8f21a4b29cbdb441f528cc
Emmanuel FARHI <farhi@ill.fr>
2018-06-26 12:59:42 +0200

    cogen3: mcstas comps: fix to comply with new syntax/declaration in comps.

--- mcstas-comps/share/ESS_butterfly-geometry.c
+++ mcstas-comps/share/ESS_butterfly-geometry.c
@@ -13,177 +13,187 @@ double butterfly_e_z2[]= {-1.8501e-01, -1.2719e-01, -1.8501e-01};
 double butterfly_e_x2[]= {3.3156e-05,  5.8985e-02,  1.1701e-01};
    
 
+void butterfly_geometry(double Bdelta_y, int Bjmax, double Bcx, double Bcz,
+  double Borientation_angle, double *BBeamlines, double Btx, double Bty, double Btz,
+  double BrC1_x, double BrC1_z, 
+  double BrC2_x, double BrC2_z, 
+  double BrC3_x, double BrC3_z,  
+  double BrT1_x, double BrT1_z, 
+  double BrT2_x, double BrT2_z, 
+  double BrT3_x, double BrT3_z,
+  double Br11, double Br12, double Br21, double Br22,
+  double Bfocus_xw, double Bfocus_yh)
+{
+  /* Draw the two butterfly shapes at top and bottom level */
+  double y0;
+  int j;
+  double rAx,rAz,rBx,rBz;
 
-/* Draw the two butterfly shapes at top and bottom level */
-magnify("");
-double y0;
-int j;
-double rAx,rAz,rBx,rBz;
+  for (y0=-Bdelta_y; y0<2*Bdelta_y; y0+=2*Bdelta_y) {
+    for (j=0; j<63; j++) {
+      
+      rAx = Br11*(butterfly_z[j]-Bcz) + Br12*(butterfly_x[j]-Bcx);
+      rAz = Br21*(butterfly_z[j]-Bcz) + Br22*(butterfly_x[j]-Bcx);
 
-for (y0=-delta_y; y0<2*delta_y; y0+=2*delta_y) {
-  for (j=0; j<63; j++) {
-    
-    rAx = r11*(butterfly_z[j]-cz) + r12*(butterfly_x[j]-cx);
-    rAz = r21*(butterfly_z[j]-cz) + r22*(butterfly_x[j]-cx);
+      rBx = Br11*(butterfly_z[j+1]-Bcz) + Br12*(butterfly_x[j+1]-Bcx);
+      rBz = Br21*(butterfly_z[j+1]-Bcz) + Br22*(butterfly_x[j+1]-Bcx);
 
-    rBx = r11*(butterfly_z[j+1]-cz) + r12*(butterfly_x[j+1]-cx);
-    rBz = r21*(butterfly_z[j+1]-cz) + r22*(butterfly_x[j+1]-cx);
+      line(rAx, y0, rAz, rBx, y0, rBz);
 
-    line(rAx, y0, rAz, rBx, y0, rBz);
-
+    }
   }
-}
 
-/* Draw the "border" between the thermal and cold areas */
-for (y0=-delta_y; y0<2*delta_y; y0+=2*delta_y) {
-  for (j=0; j<2; j++) {
-    
-    rAx = r11*(butterfly_e_z1[j]-cz) + r12*(butterfly_e_x1[j]-cx);
-    rAz = r21*(butterfly_e_z1[j]-cz) + r22*(butterfly_e_x1[j]-cx);
+  /* Draw the "border" between the thermal and cold areas */
+  for (y0=-Bdelta_y; y0<2*Bdelta_y; y0+=2*Bdelta_y) {
+    for (j=0; j<2; j++) {
+      
+      rAx = Br11*(butterfly_e_z1[j]-Bcz) + Br12*(butterfly_e_x1[j]-Bcx);
+      rAz = Br21*(butterfly_e_z1[j]-Bcz) + Br22*(butterfly_e_x1[j]-Bcx);
 
-    rBx = r11*(butterfly_e_z1[j+1]-cz) + r12*(butterfly_e_x1[j+1]-cx);
-    rBz = r21*(butterfly_e_z1[j+1]-cz) + r22*(butterfly_e_x1[j+1]-cx);
+      rBx = Br11*(butterfly_e_z1[j+1]-Bcz) + Br12*(butterfly_e_x1[j+1]-Bcx);
+      rBz = Br21*(butterfly_e_z1[j+1]-Bcz) + Br22*(butterfly_e_x1[j+1]-Bcx);
 
-    line(rAx, y0, rAz, rBx, y0, rBz);
+      line(rAx, y0, rAz, rBx, y0, rBz);
 
-    rAx = r11*(butterfly_e_z2[j]-cz) + r12*(butterfly_e_x2[j]-cx);
-    rAz = r21*(butterfly_e_z2[j]-cz) + r22*(butterfly_e_x2[j]-cx);
+      rAx = Br11*(butterfly_e_z2[j]-Bcz) + Br12*(butterfly_e_x2[j]-Bcx);
+      rAz = Br21*(butterfly_e_z2[j]-Bcz) + Br22*(butterfly_e_x2[j]-Bcx);
 
-    rBx = r11*(butterfly_e_z2[j+1]-cz) + r12*(butterfly_e_x2[j+1]-cx);
-    rBz = r21*(butterfly_e_z2[j+1]-cz) + r22*(butterfly_e_x2[j+1]-cx);
+      rBx = Br11*(butterfly_e_z2[j+1]-Bcz) + Br12*(butterfly_e_x2[j+1]-Bcx);
+      rBz = Br21*(butterfly_e_z2[j+1]-Bcz) + Br22*(butterfly_e_x2[j+1]-Bcx);
 
-    line(rAx, y0, rAz, rBx, y0, rBz);
+      line(rAx, y0, rAz, rBx, y0, rBz);
+    }
   }
+
+  /* Indicate the emission planes of cold/thermal moderator */
+  for (y0=-Bdelta_y; y0<2*Bdelta_y; y0+=2*Bdelta_y) {
+    dashed_line(BrC1_x, y0, BrC1_z, BrC2_x, y0, BrC2_z, 11);
+    dashed_line(BrC1_x, y0, BrC1_z, BrC3_x, y0, BrC3_z, 11);
+    dashed_line(BrT1_x, y0, BrT1_z, BrT2_x, y0, BrT2_z, 11);
+    dashed_line(BrT1_x, y0, BrT1_z, BrT3_x, y0, BrT3_z, 11);
+  }
+  dashed_line(BrC1_x, -Bdelta_y, BrC1_z, BrC1_x, Bdelta_y, BrC1_z, 11);
+  dashed_line(BrC2_x, -Bdelta_y, BrC2_z, BrC2_x, Bdelta_y, BrC2_z, 11);
+  dashed_line(BrC3_x, -Bdelta_y, BrC3_z, BrC3_x, Bdelta_y, BrC3_z, 11);
+  dashed_line(BrT1_x, -Bdelta_y, BrT1_z, BrT1_x, Bdelta_y, BrT1_z, 11);
+  dashed_line(BrT2_x, -Bdelta_y, BrT2_z, BrT2_x, Bdelta_y, BrT2_z, 11);
+  dashed_line(BrT3_x, -Bdelta_y, BrT3_z, BrT3_x, Bdelta_y, BrT3_z, 11);
+
+
+  /* Arrow indicating proton beam direction */
+  double ax,az,bx,bz,bbx,bbz,cBcx,cBcz;
+  az = -0.0925-Bcz;
+  ax = 0.0585-Bcx;
+  bz = -0.0925-Bcz;
+  bx = 0.0585+6-Bcx;
+  bbx = 0.0585+0.1-Bcx;
+  bbz = -0.0925+0.03-Bcz;
+  cBcx = 0.0585+0.1-Bcx;
+  cBcz = -0.0925-0.03-Bcz;
+  /* rAx,0,rAz is the centre of the moderator */
+  rAx = Br11*(az) + Br12*(ax);
+  rAz = Br21*(az) + Br22*(ax);
+  rBx = Br11*(bz) + Br12*(bx);
+  rBz = Br21*(bz) + Br22*(bx);
+  /* Main part of the arrow */
+  line(rAx, 0, rAz, rBx, 0, rBz);
+  /* Inclined lines for arrow head */
+  rBx = Br11*(bbz) + Br12*(bbx);
+  rBz = Br21*(bbz) + Br22*(bbx);
+  line(rAx, 0, rAz, rBx, 0, rBz);
+  rBx = Br11*(cBcz) + Br12*(cBcx);
+  rBz = Br21*(cBcz) + Br22*(cBcx);
+  line(rAx, 0, rAz, rBx, 0, rBz);
+
+  /* 120 degree "end of sector" lines */
+  bbz = 2 * cos(DEG2RAD*61);
+  bbx = 2 * sin(DEG2RAD*61);
+  cBcz = 2 * cos(-DEG2RAD*61);
+  cBcx = 2 * sin(-DEG2RAD*61);
+  rBx = Br11*(bbz) + Br12*(bbx);
+  rBz = Br21*(bbz) + Br22*(bbx);
+  dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
+  rBx = Br11*(cBcz) + Br12*(cBcx);
+  rBz = Br21*(cBcz) + Br22*(cBcx);
+  dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
+  bbz = 2 * cos(DEG2RAD*119);
+  bbx = 2 * sin(DEG2RAD*119);
+  cBcz = 2 * cos(-DEG2RAD*119);
+  cBcx = 2 * sin(-DEG2RAD*119);
+  rBx = Br11*(bbz) + Br12*(bbx);
+  rBz = Br21*(bbz) + Br22*(bbx);
+  dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
+  rBx = Br11*(cBcz) + Br12*(cBcx);
+  rBz = Br21*(cBcz) + Br22*(cBcx);
+  dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
+  /* Circles indicating extent of the "empBty" zone where optics is not allowed */
+  circle("xz", rAx, 0, rAz, 2.0);
+  circle("xz", rAx, -0.1, rAz, 2.0);
+  circle("xz", rAx, 0.1, rAz, 2.0);
+
+  /* Circles indicating extent of the target monolith */
+  circle("xz", rAx, 0, rAz, 5.5);
+  circle("xz", rAx, -1, rAz, 5.5);
+  circle("xz", rAx, 1, rAz, 5.5);
+
+  /* Beamport "plug" dimensions */
+  double w1=0.206/2.0, w2=0.276/2.0, l1=2.0+rAz, l2=2.0+rAz+1.75, l3=2.0+rAz+3.5;
+  line(w1, 0, l1, w1, 0, l2);
+  line(-w1, 0, l1, -w1, 0, l2);
+  line(w1, 0, l2, w2, 0, l2);
+  line(-w1, 0, l2, -w2, 0, l2);
+  line(w2, 0, l2, w2, 0, l3);
+  line(-w2, 0, l2, -w2, 0, l3);
+
+  /* Draw all the BBeamlines in "this sector" +1 */
+  double xx1, yy1, zz1, xx2, yy2, zz2, delta_omega;
+  for (j=0; j<Bjmax+1; j++) {
+    delta_omega = Borientation_angle - BBeamlines[j];
+    Br11 = cos(DEG2RAD*delta_omega);
+    Br12 = -sin(DEG2RAD*delta_omega);
+    Br21 = sin(DEG2RAD*delta_omega);
+    Br22 = cos(DEG2RAD*delta_omega);
+    xx1 = Br11*(w1) + Br12*(l1);
+    zz1 = Br21*(w1) + Br22*(l1);
+    xx2 = Br11*(w1) + Br12*(l2);
+    zz2 = Br21*(w1) + Br22*(l2);
+    dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+    xx1 = Br11*(-w1) + Br12*(l1);
+    zz1 = Br21*(-w1) + Br22*(l1);
+    xx2 = Br11*(-w1) + Br12*(l2);
+    zz2 = Br21*(-w1) + Br22*(l2);
+    dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+    xx1 = Br11*(w2) + Br12*(l2);
+    zz1 = Br21*(w2) + Br22*(l2);
+    xx2 = Br11*(w2) + Br12*(l3);
+    zz2 = Br21*(w2) + Br22*(l3);
+    dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+    xx1 = Br11*(-w2) + Br12*(l2);
+    zz1 = Br21*(-w2) + Br22*(l2);
+    xx2 = Br11*(-w2) + Br12*(l3);
+    zz2 = Br21*(-w2) + Br22*(l3);
+    dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
+  }
+
+  /* Show instrument axis... */
+  dashed_line(0,0,0,0,0,2+rAz,21);
+
+  /* Draw up the "focusing rectangle" */ 
+  /* Horizontal direction vector @ focusing area */
+  vec_prod(xx1,yy1,zz1,Btx,Bty,Btz,0.0,1.0,0.0);
+  NORM(xx1,yy1,zz1);
+  vec_prod(xx2,yy2,zz2,Btx,Bty,Btz,xx1,yy1,zz1);
+  NORM(xx2,yy2,zz2);
+  xx1*=Bfocus_xw/2.0; yy1*=Bfocus_xw/2.0; zz1*=Bfocus_xw/2.0;
+  xx2*=Bfocus_yh/2.0; yy2*=Bfocus_yh/2.0; zz2*=Bfocus_yh/2.0;
+  printf("Normal vectors pointing in directions\n %g %g %g and \n %g %g %g \n",xx1,yy1,zz1,xx2,yy2,zz2);
+  dashed_line(Btx -xx1 -xx2, Bty -yy1 -yy2, Btz -zz1 -zz2,
+	      Btx +xx1 -xx2, Bty +yy1 -yy2, Btz +zz1 -zz2,5);
+  dashed_line(Btx -xx1 +xx2, Bty -yy1 +yy2, Btz -zz1 +zz2,
+	      Btx +xx1 +xx2, Bty +yy1 +yy2, Btz +zz1 +zz2,5);
+
+  dashed_line(Btx -xx1 -xx2, Bty -yy1 -yy2, Btz -zz1 -zz2,
+	      Btx -xx1 +xx2, Bty -yy1 +yy2, Btz -zz1 +zz2,5);
+  dashed_line(Btx +xx1 -xx2, Bty +yy1 -yy2, Btz +zz1 -zz2,
+	      Btx +xx1 +xx2, Bty +yy1 +yy2, Btz +zz1 +zz2,5);
 }
-
-/* Indicate the emission planes of cold/thermal moderator */
-for (y0=-delta_y; y0<2*delta_y; y0+=2*delta_y) {
-  dashed_line(rC1_x, y0, rC1_z, rC2_x, y0, rC2_z, 11);
-  dashed_line(rC1_x, y0, rC1_z, rC3_x, y0, rC3_z, 11);
-  dashed_line(rT1_x, y0, rT1_z, rT2_x, y0, rT2_z, 11);
-  dashed_line(rT1_x, y0, rT1_z, rT3_x, y0, rT3_z, 11);
-}
-dashed_line(rC1_x, -delta_y, rC1_z, rC1_x, delta_y, rC1_z, 11);
-dashed_line(rC2_x, -delta_y, rC2_z, rC2_x, delta_y, rC2_z, 11);
-dashed_line(rC3_x, -delta_y, rC3_z, rC3_x, delta_y, rC3_z, 11);
-dashed_line(rT1_x, -delta_y, rT1_z, rT1_x, delta_y, rT1_z, 11);
-dashed_line(rT2_x, -delta_y, rT2_z, rT2_x, delta_y, rT2_z, 11);
-dashed_line(rT3_x, -delta_y, rT3_z, rT3_x, delta_y, rT3_z, 11);
-
-
-/* Arrow indicating proton beam direction */
-double ax,az,bx,bz,bbx,bbz,ccx,ccz;
-az = -0.0925-cz;
-ax = 0.0585-cx;
-bz = -0.0925-cz;
-bx = 0.0585+6-cx;
-bbx = 0.0585+0.1-cx;
-bbz = -0.0925+0.03-cz;
-ccx = 0.0585+0.1-cx;
-ccz = -0.0925-0.03-cz;
-/* rAx,0,rAz is the centre of the moderator */
-rAx = r11*(az) + r12*(ax);
-rAz = r21*(az) + r22*(ax);
-rBx = r11*(bz) + r12*(bx);
-rBz = r21*(bz) + r22*(bx);
-/* Main part of the arrow */
-line(rAx, 0, rAz, rBx, 0, rBz);
-/* Inclined lines for arrow head */
-rBx = r11*(bbz) + r12*(bbx);
-rBz = r21*(bbz) + r22*(bbx);
-line(rAx, 0, rAz, rBx, 0, rBz);
-rBx = r11*(ccz) + r12*(ccx);
-rBz = r21*(ccz) + r22*(ccx);
-line(rAx, 0, rAz, rBx, 0, rBz);
-
-/* 120 degree "end of sector" lines */
-bbz = 2 * cos(DEG2RAD*61);
-bbx = 2 * sin(DEG2RAD*61);
-ccz = 2 * cos(-DEG2RAD*61);
-ccx = 2 * sin(-DEG2RAD*61);
-rBx = r11*(bbz) + r12*(bbx);
-rBz = r21*(bbz) + r22*(bbx);
-dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
-rBx = r11*(ccz) + r12*(ccx);
-rBz = r21*(ccz) + r22*(ccx);
-dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
-bbz = 2 * cos(DEG2RAD*119);
-bbx = 2 * sin(DEG2RAD*119);
-ccz = 2 * cos(-DEG2RAD*119);
-ccx = 2 * sin(-DEG2RAD*119);
-rBx = r11*(bbz) + r12*(bbx);
-rBz = r21*(bbz) + r22*(bbx);
-dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
-rBx = r11*(ccz) + r12*(ccx);
-rBz = r21*(ccz) + r22*(ccx);
-dashed_line(rAx, 0, rAz, rBx+rAx, 0, rBz+rAz,51);
-/* Circles indicating extent of the "empty" zone where optics is not allowed */
-circle("xz", rAx, 0, rAz, 2.0);
-circle("xz", rAx, -0.1, rAz, 2.0);
-circle("xz", rAx, 0.1, rAz, 2.0);
-
-/* Circles indicating extent of the target monolith */
-circle("xz", rAx, 0, rAz, 5.5);
-circle("xz", rAx, -1, rAz, 5.5);
-circle("xz", rAx, 1, rAz, 5.5);
-
-/* Beamport "plug" dimensions */
-double w1=0.206/2.0, w2=0.276/2.0, l1=2.0+rAz, l2=2.0+rAz+1.75, l3=2.0+rAz+3.5;
-line(w1, 0, l1, w1, 0, l2);
-line(-w1, 0, l1, -w1, 0, l2);
-line(w1, 0, l2, w2, 0, l2);
-line(-w1, 0, l2, -w2, 0, l2);
-line(w2, 0, l2, w2, 0, l3);
-line(-w2, 0, l2, -w2, 0, l3);
-
-/* Draw all the beamlines in "this sector" +1 */
-double xx1, yy1, zz1, xx2, yy2, zz2, delta_omega;
-for (j=0; j<jmax+1; j++) {
-  delta_omega = orientation_angle - Beamlines[j];
-  r11 = cos(DEG2RAD*delta_omega);
-  r12 = -sin(DEG2RAD*delta_omega);
-  r21 = sin(DEG2RAD*delta_omega);
-  r22 = cos(DEG2RAD*delta_omega);
-  xx1 = r11*(w1) + r12*(l1);
-  zz1 = r21*(w1) + r22*(l1);
-  xx2 = r11*(w1) + r12*(l2);
-  zz2 = r21*(w1) + r22*(l2);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-  xx1 = r11*(-w1) + r12*(l1);
-  zz1 = r21*(-w1) + r22*(l1);
-  xx2 = r11*(-w1) + r12*(l2);
-  zz2 = r21*(-w1) + r22*(l2);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-  xx1 = r11*(w2) + r12*(l2);
-  zz1 = r21*(w2) + r22*(l2);
-  xx2 = r11*(w2) + r12*(l3);
-  zz2 = r21*(w2) + r22*(l3);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-  xx1 = r11*(-w2) + r12*(l2);
-  zz1 = r21*(-w2) + r22*(l2);
-  xx2 = r11*(-w2) + r12*(l3);
-  zz2 = r21*(-w2) + r22*(l3);
-  dashed_line(xx1, 0, zz1, xx2, 0, zz2, 11);
-}
-
-/* Show instrument axis... */
-dashed_line(0,0,0,0,0,2+rAz,21);
-
-/* Draw up the "focusing rectangle" */ 
-/* Horizontal direction vector @ focusing area */
-vec_prod(xx1,yy1,zz1,tx,ty,tz,0.0,1.0,0.0);
-NORM(xx1,yy1,zz1);
-vec_prod(xx2,yy2,zz2,tx,ty,tz,xx1,yy1,zz1);
-NORM(xx2,yy2,zz2);
-xx1*=focus_xw/2.0; yy1*=focus_xw/2.0; zz1*=focus_xw/2.0;
-xx2*=focus_yh/2.0; yy2*=focus_yh/2.0; zz2*=focus_yh/2.0;
-printf("Normal vectors pointing in directions\n %g %g %g and \n %g %g %g \n",xx1,yy1,zz1,xx2,yy2,zz2);
-dashed_line(tx -xx1 -xx2, ty -yy1 -yy2, tz -zz1 -zz2,
-	    tx +xx1 -xx2, ty +yy1 -yy2, tz +zz1 -zz2,5);
-dashed_line(tx -xx1 +xx2, ty -yy1 +yy2, tz -zz1 +zz2,
-	    tx +xx1 +xx2, ty +yy1 +yy2, tz +zz1 +zz2,5);
-
-dashed_line(tx -xx1 -xx2, ty -yy1 -yy2, tz -zz1 -zz2,
-	    tx -xx1 +xx2, ty -yy1 +yy2, tz -zz1 +zz2,5);
-dashed_line(tx +xx1 -xx2, ty +yy1 -yy2, tz +zz1 -zz2,
-	    tx +xx1 +xx2, ty +yy1 +yy2, tz +zz1 +zz2,5);
